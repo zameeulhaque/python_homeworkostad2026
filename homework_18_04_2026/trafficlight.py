81c2577bdfd822bfe8e81421c33362728e5a3611
zameeulhaque <zameeeverforever@gmail.com>
2026-04-22 21:34:52 +0600

    Update trafficlight.py

--- homework_18_04_2026/trafficlight.py
+++ homework_18_04_2026/trafficlight.py
@@ -1,1 +1,11 @@
 
+color = input("Enter traffic light color: ").strip().lower()
+
+if color == "red":
+    print("Stop!")
+elif color == "yellow":
+    print("Wait!")
+elif color == "green":
+    print("Go!")
+else:
+    print("Invalid color!")
