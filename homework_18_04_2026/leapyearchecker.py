ecb728bdd5ee350617c6c3b3eb37efacf763210d
zameeulhaque <zameeeverforever@gmail.com>
2026-04-22 21:11:22 +0600

    Update leapyearchecker.py

--- homework_18_04_2026/leapyearchecker.py
+++ homework_18_04_2026/leapyearchecker.py
@@ -1,1 +1,14 @@
 
+try:
+    year = int(input("Please Enter Year : "))
+    if year % 400 == 0:
+        print(f"leap Year {year}")
+    elif year % 100 == 0:
+        print(f"Not Leap Year {year}")
+    elif year % 4 == 0:
+        print(f"Leap Year {year}")
+    else:
+        print("Not Leap Year")
+except ValueError:
+    print("Invalid Input")
+             
