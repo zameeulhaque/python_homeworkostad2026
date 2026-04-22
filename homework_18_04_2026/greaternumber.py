fdc7b5346ace3d59657594add8e44da4a31d4448
zameeulhaque <zameeeverforever@gmail.com>
2026-04-22 21:54:52 +0600

    Update greaternumber.py

--- homework_18_04_2026/greaternumber.py
+++ homework_18_04_2026/greaternumber.py
@@ -1,1 +1,12 @@
 
+number1 = float(input("Please Enter First Number : "))
+number2 = float(input("Please Enter Second Number : "))
+if number1 > number2:
+    print(f"{number1} is greater than {number2}")
+elif number1 < number2:
+    print(f"{number2} is greater than {number1}")
+else:
+    print("Equal Number")
+
+    
+    
